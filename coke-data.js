b96d7d98f7b47a78f2542a1a92270cf6f7f8c780
jasontal1 <jason@jasontal.com>
2026-07-18 14:13:27 -0400

    Add files via upload

---
+++ coke-data.js
@@ -0,0 +1,14 @@
+window.STICKER_DATA = [
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "1", "name": "Lamine Yamal - Spain", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "2", "name": "Joshua Kimmich - Germany", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "3", "name": "Harry Kane - England", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "4", "name": "Santiago Gimenez - Mexico", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "5", "name": "Antonee Robinson - USA", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "6", "name": "Jefferson Lerma - Colombia", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "7", "name": "Edson Alvarez - Mexico", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "8", "name": "Virgil van Dijk - Netherlands", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "9", "name": "Alphonso Davies - Canada", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "10", "name": "Weston McKennie - USA", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "11", "name": "Lautaro Martinez - Argentina", "type": "coca-cola promo"},
+  {"team": "Coca-Cola Promo", "code": "CC", "num": "12", "name": "Gabriel Magalhaes - Brazil", "type": "coca-cola promo"}
+];
